e6c3f5d5d20f6c18fb5f8020dd0bc3659cf741b5
Charlesson <charlesssantos93@gmail.com>
2022-07-25 11:04:31 -0300

    Inserindo listas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+   assistidos: Array <string> = ['p1', 'p3', 'p5', 'p6', 'p7', 'p8', 'p9' ];
+   favoritos: Array <string> = ['p10', 'p2', 'p12', 'p10', 'p11', 'p13', 'p14'];
   constructor() { }
 
   ngOnInit(): void {
